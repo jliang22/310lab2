f1d39cccb43eaf8e84dfb6ac5067409fa42c591b
Norman Sue <normful@gmail.com>
2014-09-22 18:49:36 -0700

    Change greeting in server/GreetingServiceImpl.java

--- src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
+++ src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
@@ -12,7 +12,7 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
     GreetingService {
 
   public String greetServer(String input) throws IllegalArgumentException {
-    // Verify that the input is valid. 
+    // Verify that the input is valid.
     if (!FieldVerifier.isValidName(input)) {
       // If the input is not valid, throw an IllegalArgumentException back to
       // the client.
@@ -27,14 +27,14 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
     input = escapeHtml(input);
     userAgent = escapeHtml(userAgent);
 
-    return "Hello, " + input + "!<br><br>I am running " + serverInfo
-        + ".<br><br>It looks like you are using:<br>" + userAgent;
+    return "Hello, " + input + "!<br><br>I am running this server: " + serverInfo
+        + ".<br><br>It looks like you are using this user agent: " + userAgent;
   }
 
   /**
    * Escape an html string. Escaping data received from the client helps to
    * prevent cross-site script vulnerabilities.
-   * 
+   *
    * @param html the html string to escape
    * @return the escaped string
    */
